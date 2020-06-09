49a95b19abc7fedd32f29953b0a4a5ca542d01f6
atk-exilio <sandoval.lucasj@gmail.com>
2020-06-09 19:23:40 -0300

    Add - CI test

---
+++ src/test/java/com/coronavirus/insumos/PruebaTest.java
@@ -0,0 +1,14 @@
+package com.coronavirus.insumos;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PruebaTest {
+
+	@Test
+	public void ciTest() {
+		assertTrue(true);
+	}
+
+}
