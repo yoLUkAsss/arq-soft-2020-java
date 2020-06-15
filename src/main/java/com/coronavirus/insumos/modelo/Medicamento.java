3ffc0a197b188c417e5e444bbf7b108e326ac58a
fermartinotti <fermartinotti@gmail.com>
2020-06-15 13:32:18 -0300

    Fix Medicamento annotations.

--- src/main/java/com/coronavirus/insumos/modelo/Medicamento.java
+++ src/main/java/com/coronavirus/insumos/modelo/Medicamento.java
@@ -4,20 +4,10 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+
 
 @Entity
 @JsonIgnoreProperties(value = {"new", "id"})
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
-@JsonSubTypes({
-		@Type(value = Enviado.class, name= "Enviado"),
-		@Type(value = Cancelado.class, name= "Cancelado"),
-		@Type(value = Aprobado.class, name= "Aprobado"),
-		@Type(value = Rechazado.class, name= "Rechazado"),
-		}
-)
 public class Medicamento extends Insumo {
 
 	@Column
